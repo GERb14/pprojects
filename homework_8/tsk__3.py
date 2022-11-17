a924b6bc694f9393b593c2ff14e4fd27aa6b727d
GERb14 <116296180+GERb14@users.noreply.github.com>
2022-11-17 21:24:10 +0200

    hm8

--- homework_8/tsk__3.py
+++ homework_8/tsk__3.py
@@ -1,10 +1,13 @@
 value = [1, 2, 5, 7, 9, 99, 87, 200, 39, 2]
 value1 = [5, 42, 29, 345, 50, 33, 7, 0, 201, 9, 2, 132, 45, 23, 934]
 var = [i for i in value if i in value1]
-var1 = set(var)
-for s in var1:
-    if s != 2:
-        print(s, "- 1 time")
-    else:
+var.sort()
+cnt = -1
+for s in var:
+    count = cnt + 1
+    if s == var[count]:
+        var.remove(s)
         print(s, "- 2 times")
-# Я не понял как доказать то, что у меня два повторения цифры 2
+    else:
+        print(s, "- 1 time")
+
