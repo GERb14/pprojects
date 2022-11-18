127bdf51f57be94debc0b77c145c4d66c425f6b8
GERb14 <116296180+GERb14@users.noreply.github.com>
2022-11-18 11:31:39 +0200

    hm7

--- homework_8/tsk__2.py
+++ homework_8/tsk__2.py
@@ -1,10 +1,10 @@
 var = input("Write a sentence, using more than 2 words:")
 var1 = var.split(" ")
 var1.sort()
-var2 = len(var1)
 if var.count(" ") >= 2:
     for i in range(var1.count("")):
         var1.remove("")
+var2 = len(var1)
 cnt = -1
 if var2 > 2:
     for s in var1:
