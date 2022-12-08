d30dcb764fc1a25b0442999c483d648d6ded4e21
GERb14 <116296180+GERb14@users.noreply.github.com>
2022-12-08 20:38:01 +0200

    w

--- homework_9/task__2.py
+++ homework_9/task__2.py
@@ -4,9 +4,7 @@ var = [[random.randint(1, 10) for i in range(N)] for k in range(N)]
 print(var)
 dgn_sum = 0
 for i in range(N):
-    for k in range(N):
-        if i == k:
-            dgn_sum += var[i][k]
+    dgn_sum += var[i][i]
 var1 = [i.pop() for i in var]
 print("last column numbers sum:", sum(var1))
 print("sum of numbers on diagonal:", dgn_sum)
