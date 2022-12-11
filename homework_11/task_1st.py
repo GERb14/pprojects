772ba41b92c15513bff45d769aa5d51547d7531b
GERb14 <116296180+GERb14@users.noreply.github.com>
2022-12-11 14:17:13 +0200

    hm12

--- homework_11/task_1st.py
+++ homework_11/task_1st.py
@@ -3,14 +3,14 @@ import random
 
 def numbers_sum(some_list, sum_of_numbers):
     my_result = False
-    for aa in some_list:
-        for bb in some_list:
-            if aa + bb == sum_of_numbers:
+    for aa in range(len(some_list)):
+        for bb in range(aa + 1, len(some_list)):
+            if sum([some_list[aa], some_list[bb]]) == sum_of_numbers:
                 my_result = True
     return my_result
 
 
-lst = [random.randint(1, 100) for i in range(1, 50)]
+lst = [random.randint(1, 100) for i in range(0, 20)]
 number = random.randint(1, 100)
 result = numbers_sum(lst, number)
 print(result)
--- homework_11/task_3rd.py
+++ homework_11/task_3rd.py
@@ -2,16 +2,14 @@ import random
 
 
 def prime_numbers(n, z):
-    lst = []
     for i in range(n, z):
         d = 2
         while i % d != 0:
             d += 1
         if d == i:
-            lst.append(i)
-    return lst
+            yield i
 
 
 end = random.randint(1, 100)
-result = prime_numbers(2, end)
-print(result)
+for s in prime_numbers(2, end):
+    print(s, end=' ')
