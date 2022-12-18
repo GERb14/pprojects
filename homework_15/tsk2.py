836a1a130aa33442ad1d6614b09b9c967973d40c
GERb14 <116296180+GERb14@users.noreply.github.com>
2022-12-18 15:13:22 +0200

    hm16 files updated

--- homework_15/tsk2.py
+++ homework_15/tsk2.py
@@ -1,11 +1,10 @@
 def longest_words(file):
-    my_file = open(file, 'r', encoding='utf-8')
-    my_file1 = my_file.read().replace('\"', ' ').split()
-    lst2 = [len(i) for i in my_file1]
-    for j in range(len(my_file1)):
-        if lst2[j] == max(lst2):
-            print("{:>10}".format(my_file1[j], emd=' '))
-    return
+    with open(file, 'r', encoding='utf-8') as my_file:
+        my_file1 = my_file.read().replace('\"', ' ').split()
+        lst2 = [len(i) for i in my_file1]
+        for j in range(len(my_file1)):
+            if lst2[j] == max(lst2):
+                print("{:>10}".format(my_file1[j], emd=' '))
 
 
 if __name__ == '__main__':
